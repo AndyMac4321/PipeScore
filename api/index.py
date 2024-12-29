0fec9a7924c495a597b467f097397c48d3d9c991
AndyMac4321 <76612952+AndyMac4321@users.noreply.github.com>
2024-12-28 19:41:57 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,12 @@
 from http.server import BaseHTTPRequestHandler
-import re
- 
+import re 
 class handler(BaseHTTPRequestHandler):
  
-     
-       def do_GET(self):
+    
+    def do_GET(self):
         if self.path.startswith("/pipescore"):
             self.path = "/pipescore.html"
         elif self.path == "/":
             self.path = "/index.html"
-        elif "." not in re.search("(/.*?)$", self.path).group(0):
-            self.path += ".html"
 
         return super().do_GET()
