b75f9f5834680521672e250dc9d68c357bfeaa44
AndyMac4321 <76612952+AndyMac4321@users.noreply.github.com>
2024-12-28 19:33:18 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,15 @@
 from http.server import BaseHTTPRequestHandler
+import re
  
 class handler(BaseHTTPRequestHandler):
  
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+     
+       def do_GET(self):
+        if self.path.startswith("/pipescore"):
+            self.path = "/pipescore.html"
+        elif self.path == "/":
+            self.path = "/index.html"
+        elif "." not in re.search("(/.*?)$", self.path).group(0):
+            self.path += ".html"
+
+        return super().do_GET()
